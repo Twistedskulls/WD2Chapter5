ec6defbe76c5c033caabdcebab62e16028a93515
Twistedskulls <tootsiemomma@gmail.com>
2026-02-28 14:43:14 -0600

    k

--- calculator.js
+++ calculator.js
@@ -6,3 +6,7 @@ function calculate()
 {
     document.getElementById('display').value = eval(document.getElementById('display').value);
 }
+function clearMe()
+{
+    document.getElementById('display').value = '';
+}
