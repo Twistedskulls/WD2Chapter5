17022d810ebeb0c0fc680e04c245c47be21ebf16
Twistedskulls <tootsiemomma@gmail.com>
2026-02-28 14:34:33 -0600

    cal

--- calculator.js
+++ calculator.js
@@ -1,5 +1,8 @@
 function displayMe(inVal)
 {
-    document.getElementById('display').value =
-    document.getElementById('display').value + inVal;
+    document.getElementById('display').value = document.getElementById('display').value + inVal;
+}
+function calculate()
+{
+    document.getElementById('display').value = eval(document.getElementById('display').value);
 }
