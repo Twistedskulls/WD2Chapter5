d865467dae60dd43fee87b8284b935672764a98f
Twistedskulls <tootsiemomma@gmail.com>
2026-03-02 17:53:38 -0600

    copies

---
+++ stoplight2.js
@@ -0,0 +1,12 @@
+function makeGreenLight ()
+{
+    document.getElementById("GreenLight").style.background = "green";
+}
+function makeRedLight ()
+{
+    document.getElementById("RedLight").style.background = "red";
+}
+function makeYellowLight ()
+{
+    document.getElementById("YellowLight").style.background = "yellow";
+}
