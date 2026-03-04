7a134250a20fe2d76b5f2103d973b91ca8f8d107
Twistedskulls <tootsiemomma@gmail.com>
2026-03-04 17:06:13 -0600

    calc

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -8,3 +8,17 @@ function calculate()
     document.getElementById('display').value =
     eval(document.getElementById('display').value);
 }
+function clearMe()
+{
+    document.getElementById('display').value = "";
+}
+function switchSigns()
+{
+    document.getElementById('display').value =
+        docoument.getElementById('display').value * -1;
+}
+function percentMe()
+{
+    document.getElementById('display').value =
+        document.getElementById('display').value / 100;
+}
