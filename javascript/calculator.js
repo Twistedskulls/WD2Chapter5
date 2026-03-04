b2ded1cab96db892d901649468aaa015b342e741
Twistedskulls <tootsiemomma@gmail.com>
2026-03-04 16:58:26 -0600

    calculate

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -3,3 +3,8 @@ function displayMe(inVal)
     document.getElementById('display').value =
     document.getElementById('display').value + inVal;
 }
+function calculate()
+{
+    document.getElementById('display').value =
+    eval(document.getElementById('display').value);
+}
